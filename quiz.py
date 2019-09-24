6dc7d51e85d7a1d36e12cacc0c34bdf0d66c8405
Ubuntu <ubuntu@ip-172-31-88-99.ec2.internal>
2019-09-24 13:27:34 +0000

    Add an absolute path to new file in f variable

--- quiz.py
+++ quiz.py
@@ -1,4 +1,4 @@
-f = open('data.txt', 'r')  #create a variable to open our txt file for reading.
+f = open('/home/ubuntu/environment/files/relative_data.txt', 'r')  #create a variable to open our txt file for reading.
 lines = f.read()  #here we call the readlines method and put that in the variable lines.
 f.close()  #We'll close our file handle
 print(lines) #and then print the results to the screen.
