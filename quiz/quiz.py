b06b4de7cbfeb6980fc4883fe642dcd15c5527c9
Ubuntu <ubuntu@ip-172-31-88-99.ec2.internal>
2019-09-25 12:00:51 +0000

    Add game_loop() function with if statements

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -5,3 +5,18 @@ def show_menu():
     
     option = input("Enter option: ")
     return option
+    
+def game_loop():
+    while True:   #loop until there is a break
+       option = show_menu() 
+       if option == "1":  #if our option is equal to 1, print the string.
+         print("You selected 'Ask questions'")
+       elif option == "2": 
+           print("You selected 'Add a question'")
+       elif option == "3":
+           break     #if choosing option 3, quit the game
+       else:       #In case somebody puts anything other than 1, 2 or 3  we'll print 'Invalid option'. 
+           print("Invalid option")
+           print("")   #print a blank line after all this, to make it look nicer
+           
+game_loop()   #call the game loop
