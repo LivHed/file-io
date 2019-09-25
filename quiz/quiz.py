3217cc29b6fe5d9cd2c507600a1629f91752c732
Ubuntu <ubuntu@ip-172-31-88-99.ec2.internal>
2019-09-25 12:22:15 +0000

    Add add_question function

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -5,6 +5,20 @@ def show_menu():
     
     option = input("Enter option: ")
     return option
+  
+def add_question():
+    print("")
+    question = input("Enter a question\n> ")  #We'll put a new line here, and then a greater-than sign as a prompt, and a space just to make it look nice.
+    
+    print("")
+    print("OK then, tell me the answer")
+    answer = input("{0}\n> ".format(question))  #we'll take the question that we'd already asked, do a blank line, a greater-than sign for a prompt and then, 
+                                                #using the format method, we'll insert the question there. So, now the user will be asked the question again 
+                                                #when they come to put the answer in.
+    file = open("questions.txt","a")   #we add this to the file. We'll open our questions.txt file for appending using the 'a' access mode flag.
+    file.write(question + "\n")    #we will first add our questions and add a new line
+    file.write(answer + "\n")    #The second thing we'll do is write our answer
+    file.close()  #and finally, close the file
     
 def game_loop():
     while True:   #loop until there is a break
@@ -12,7 +26,7 @@ def game_loop():
        if option == "1":  #if our option is equal to 1, print the string.
          print("You selected 'Ask questions'")
        elif option == "2": 
-           print("You selected 'Add a question'")
+           add_question()  #if the user selects option 2 then the add_question() function will be called and we'll be prompted for a question and an answer.
        elif option == "3":
            break     #if choosing option 3, quit the game
        else:       #In case somebody puts anything other than 1, 2 or 3  we'll print 'Invalid option'. 
