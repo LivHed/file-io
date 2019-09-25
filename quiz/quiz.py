464485d55ebf8f78c885d12ef6e1bda8d0216256
Ubuntu <ubuntu@ip-172-31-88-99.ec2.internal>
2019-09-25 15:16:49 +0000

    refactor

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,3 +1,5 @@
+import difflib
+
 def show_menu():
     print("1. Ask questions")
     print("2. Add question")
@@ -11,16 +13,35 @@ def ask_questions():
     answers = []
     
     with open("questions.txt", "r") as file:   # a with block is a good method for opening files.it handles a lot of stuff for us. For instance, we don't need to worry about closing the file because at the end of the width block it will be closed.
-     lines = file.read().splitlines()  #in the variable lines: read our file in and split the lines.
+      lines = file.read().splitlines()  #in the variable lines: read our file in and split the lines.
      
-     for i, text in enumerate(lines):   #The enumerate function is going to turn each of these lists into a tuple with a line number stored in 'i' and
+    for i, text in enumerate(lines):   #The enumerate function is going to turn each of these lists into a tuple with a line number stored in 'i' and
          if i%2 == 0:                   #the text in 'text'. So, if 'i' is even - if the line number is even -then we say that that's a question. 
            questions.append(text)       #If it's odd, then that's going to be an answer.
          else:
            answers.append(text)
            
-     for question, answer in zip(questions, answers):
+    number_of_questions = len(questions)   #we need to know the number of questions that there are in our file. We do that by checking the length of our questions list.
+    questions_and_answers = zip(questions, answers)  #Then, we're going to use questions and answers here as another variable and we're going to call the zip function on this. 
+                                                       #, this prevents us having to run the zip function every time the for loop is run
+    score = 0
+           
+    for question, answer in questions_and_answers:
          guess = input(question + "> ")
+         if guess == answer:   #if our guess is equal to our answer then it's correct and we add 1 to the score. We then print that we were right.
+            score += 1
+            print("right!")
+            print(score)
+         else:
+            if (sum([i[0] != ' '  for i in difflib.ndiff(guess, answer)]) / 2) <= 2:
+                print(sum([i[0] != ' '  for i in difflib.ndiff(guess, answer)]))
+                print("Spelled wrong, but right!")
+                score += 1
+                print(score)  
+            else:
+                print("wrong!")
+                
+    print("You got {0} correct out of {1}".format(score, number_of_questions))
          
 def add_question():
     print("")
@@ -40,7 +61,7 @@ def game_loop():
     while True:   #loop until there is a break
        option = show_menu() 
        if option == "1":  #if our option is equal to 1, print the string.
-         print("You selected 'Ask questions'")
+         ask_questions()
        elif option == "2": 
            add_question()  #if the user selects option 2 then the add_question() function will be called and we'll be prompted for a question and an answer.
        elif option == "3":
